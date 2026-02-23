36739ef8e77b813d822fb52b8b90c1a6b2e422d1
Andttrea <andreavg@lcg.unam.mx>
2026-02-22 20:07:28 -0600

    Add differential expression analysis for all mutations and treatment time with comprehensive visualizations and heatmap

--- code/DE_plots.R
+++ code/DE_plots.R
@@ -1,9 +1,5 @@
-# Load libraries
-library(limma)
-
-# Guardando anteriores variables 
-rse_gene_SRP127181_filtered <- readRDS(file = "processed-data/rse_gene_SRP127181_filtered")
-dge <- readRDS(file = "processed-data/dge")
+# Finally, we will examine the treatment time variable to see if there are more genes expressed over time.
+# We perform the same process as we did for the mutationsGrison between Wild Type and Mutant D538G
 model <- readRDS(file = "processed-data/model_matrix")
 
 # Voom transformation
@@ -11,14 +7,186 @@ vGene <- voom(dge, model, plot = TRUE)
 
 # Fit linear model
 eb_results <- eBayes(lmFit(vGene, model))
-# Get the top differentially expressed genes
-DE_results <- topTable(eb_results, coef = 2, number = nrow(rse_gene_SRP127181_filtered), sort.by = "none")
-dim(DE_results)
+# Get the top differentially expressed genes para la comparación entre el Wild Type y el Mutant D538G
+DE_results_D538G <- topTable(
+  eb_results,
+  coef = 2,
+  number = nrow(rse_gene_SRP127181_filtered),
+  sort.by = "none"
+)
+# We get the table dimensions to check if we didn't lose any genes in the analysis process
+dim(DE_results_D538G)
+# Output of the code above:
+# [1] 21272    27
+# Observamos que no perdimos ningún gen
 
+# Observamos DE_results
+head(DE_results_D538G)
+# Output of the code above:
+#                   source type bp_length phase           gene_id              gene_type     gene_name level
+#ENSG00000227232.5 HAVANA gene      1351    NA ENSG00000227232.5 unprocessed_pseudogene        WASH7P     2
+#ENSG00000238009.6 HAVANA gene      3726    NA ENSG00000238009.6                lincRNA  RP11-34P13.7     2
+#ENSG00000233750.3 HAVANA gene      3812    NA ENSG00000233750.3   processed_pseudogene        CICP27     1
+#ENSG00000268903.1 HAVANA gene       755    NA ENSG00000268903.1   processed_pseudogene RP11-34P13.15     2
+#ENSG00000269981.1 HAVANA gene       284    NA ENSG00000269981.1   processed_pseudogene RP11-34P13.16     2
+#ENSG00000241860.6 HAVANA gene      6195    NA ENSG00000241860.6   processed_transcript RP11-34P13.13     2
+#                           havana_gene               tag       logFC   AveExpr          t      P.Value
+#ENSG00000227232.5 OTTHUMG00000000958.1              <NA> -0.15453287  1.882552 -1.3539947 1.869565e-01
+#ENSG00000238009.6 OTTHUMG00000001096.2 overlapping_locus -0.04509432 -1.343374 -0.1727149 8.641630e-01
+#ENSG00000233750.3 OTTHUMG00000001257.3    pseudo_consens -1.04283697 -2.372336 -3.6438460 1.125553e-03
+#ENSG00000268903.1 OTTHUMG00000182518.2              <NA> -1.40238502 -1.288555 -5.8482074 3.151641e-06
+#ENSG00000269981.1 OTTHUMG00000182738.2              <NA> -0.79611373 -1.671208 -2.9636278 6.277981e-03
+#ENSG00000241860.6 OTTHUMG00000002480.3        ncRNA_host -0.08292388 -2.005890 -0.3208142 7.508197e-01
+#                     adj.P.Val          B
+#ENSG00000227232.5 2.430446e-01 -6.4310394
+#ENSG00000238009.6 8.912715e-01 -6.5727263
+#ENSG00000233750.3 2.622715e-03 -0.9341409
+#ENSG00000268903.1 1.513357e-05  4.5514185
+#ENSG00000269981.1 1.207461e-02 -2.6452264
+#ENSG00000241860.6 7.948361e-01 -6.3517654
 
+# Filter genes with P-value less than 0.05
+table(DE_results_D538G$adj.P.Val < 0.05)
+# Output of the code above:
+#FALSE  TRUE
+# 8123 13149
+# Se observa que hay 13149 genes que son significativamente diferentes entre el Wild Type y el Mutant D538G.
 
-head(DE_results)
-table(DE_results$adj.P.Val < 0.05)
+# Create an MA plot
 plotMA(eb_results, coef = 2)
 
-volcanoplot(eb_results, coef = 2, highlight = 4, names = DE_results$gene_name)
+# Create a volcano plot, highlighting the 4 most significant genes
+volcanoplot(
+  eb_results,
+  coef = 2,
+  highlight = 4,
+  names = DE_results_D538G$gene_name
+)
+
+# Lo anterior solo fue hecho para la comparación entre el Wild Type y el Mutant D538G
+# Necesitaremos comparar la otra mutación Y537S con el Wild Type, para eso haremos lo mismo que hicimos para D538G
+
+DE_results_Y537S <- topTable(
+  eb_results,
+  coef = 3,
+  number = nrow(rse_gene_SRP127181_filtered),
+  sort.by = "none"
+)
+dim(DE_results_Y537S)
+# Output of the code above:
+#[1] 21272    16
+# No genes were lost in the analysis process
+
+head(DE_results_Y537S)
+# Output of the code above:
+#   source type bp_length phase           gene_id              gene_type     gene_name level
+#ENSG00000227232.5 HAVANA gene      1351    NA ENSG00000227232.5 unprocessed_pseudogene        WASH7P     2
+#ENSG00000238009.6 HAVANA gene      3726    NA ENSG00000238009.6                lincRNA  RP11-34P13.7     2
+#ENSG00000233750.3 HAVANA gene      3812    NA ENSG00000233750.3   processed_pseudogene        CICP27     1
+#ENSG00000268903.1 HAVANA gene       755    NA ENSG00000268903.1   processed_pseudogene RP11-34P13.15     2
+#ENSG00000269981.1 HAVANA gene       284    NA ENSG00000269981.1   processed_pseudogene RP11-34P13.16     2
+#ENSG00000241860.6 HAVANA gene      6195    NA ENSG00000241860.6   processed_transcript RP11-34P13.13     2
+#                           havana_gene               tag      logFC   AveExpr         t      P.Value    adj.P.Val          B
+#ENSG00000227232.5 OTTHUMG00000000958.1              <NA> -0.4608210  1.882552 -3.383614 2.199146e-03 0.0043286966 -2.5505247
+#ENSG00000238009.6 OTTHUMG00000001096.2 overlapping_locus -0.4774108 -1.343374 -1.477651 1.510652e-01 0.1975689262 -5.6134246
+#ENSG00000233750.3 OTTHUMG00000001257.3    pseudo_consens -1.4389688 -2.372336 -4.176659 2.766436e-04 0.0006618785  0.3156886
+#ENSG00000268903.1 OTTHUMG00000182518.2              <NA> -1.2966435 -1.288555 -4.677415 7.237972e-05 0.0001974684  1.4198915
+#ENSG00000269981.1 OTTHUMG00000182738.2              <NA> -0.9573642 -1.671208 -3.092189 4.575970e-03 0.0084284378 -2.4875362
+#ENSG00000241860.6 OTTHUMG00000002480.3        ncRNA_host -0.4846160 -2.005890 -1.541983 1.347107e-01 0.1782067787 -5.3617502
+
+table(DE_results_Y537S$adj.P.Val < 0.05)
+# Output of the code above:
+# FALSE  TRUE
+# 7358 13914
+
+# Create an MA plot
+plotMA(eb_results, coef = 3)
+
+# Create a volcano plot, highlighting the 4 most significant genes
+volcanoplot(
+  eb_results,
+  coef = 3,
+  highlight = 4,
+  names = DE_results_Y537S$gene_name
+)
+
+# Por ultimo, observaremos la variable del tiempo de tratamiento, para ver si hay más genes expresados a través del tiempo.
+# Realizamos el mismo proceso que hicimos para las mutaciones
+
+DE_results_time <- topTable(
+  eb_results,
+  coef = 4,
+  number = nrow(rse_gene_SRP127181_filtered),
+  sort.by = "none"
+)
+dim(DE_results_time)
+# Output of the code above:
+# [1] 21272    16
+
+head(DE_results_time)
+# Output of the code above:
+#    source type bp_length phase           gene_id              gene_type     gene_name level
+#ENSG00000227232.5 HAVANA gene      1351    NA ENSG00000227232.5 unprocessed_pseudogene        WASH7P     2
+#ENSG00000238009.6 HAVANA gene      3726    NA ENSG00000238009.6                lincRNA  RP11-34P13.7     2
+#ENSG00000233750.3 HAVANA gene      3812    NA ENSG00000233750.3   processed_pseudogene        CICP27     1
+#ENSG00000268903.1 HAVANA gene       755    NA ENSG00000268903.1   processed_pseudogene RP11-34P13.15     2
+#ENSG00000269981.1 HAVANA gene       284    NA ENSG00000269981.1   processed_pseudogene RP11-34P13.16     2
+#ENSG00000241860.6 HAVANA gene      6195    NA ENSG00000241860.6   processed_transcript RP11-34P13.13     2
+#                           havana_gene               tag       logFC   AveExpr          t     P.Value  adj.P.Val         B
+#ENSG00000227232.5 OTTHUMG00000000958.1              <NA> -0.03177697  1.882552 -0.3887589 0.700501487 0.80553672 -6.775980
+#ENSG00000238009.6 OTTHUMG00000001096.2 overlapping_locus  0.29103545 -1.343374  1.4753110 0.151688896 0.29043440 -5.030903
+#ENSG00000233750.3 OTTHUMG00000001257.3    pseudo_consens  0.35182339 -2.372336  1.7075675 0.099185176 0.21389334 -4.528882
+#ENSG00000268903.1 OTTHUMG00000182518.2              <NA>  0.47367915 -1.288555  2.7965083 0.009399936 0.03916088 -2.738240
+#ENSG00000269981.1 OTTHUMG00000182738.2              <NA>  0.60677409 -1.671208  3.0129370 0.005563996 0.02679586 -2.207284
+#ENSG00000241860.6 OTTHUMG00000002480.3        ncRNA_host  0.24237037 -2.005890  1.2641803 0.216963649 0.37236367 -5.152552
+
+table(DE_results_time$adj.P.Val < 0.05)
+# Output of the code above:
+#FALSE  TRUE
+#15658  5614
+
+# Plots
+plotMA(eb_results, coef = 4)
+volcanoplot(
+  eb_results,
+  coef = 4,
+  highlight = 4,
+  names = DE_results_time$gene_name
+)
+
+# Finally, we will create a heatmap with the 50 most significant genes
+
+# Select the top 50 genes with the lowest overall p-value (F-statistic)
+# eb_results$F.p.value tests if ANY coefficient in the model is non-zero
+top_indices <- order(eb_results$F.p.value)[1:50]
+global_heatmap_matrix <- vGene$E[top_indices, ]
+
+# 2. Assign human-readable gene names to the rows
+# Replacing Ensembl IDs with Gene Symbols for better interpretability
+rownames(global_heatmap_matrix) <- rse_gene_SRP127181_filtered$gene_name[
+  top_indices
+]
+
+# 3. Prepare sample metadata for the heatmap annotation
+# We include all variables to see how they influence the clustering
+metadata_annotation <- as.data.frame(colData(rse_gene_SRP127181_filtered)[,
+  c(
+    "sra_attribute.mutation_status",
+    "sra_attribute.treatment_time",
+    "sra_attribute.treatment"
+  )
+])
+
+# Rename columns for a cleaner legend in the plot
+colnames(metadata_annotation) <- c("Mutation", "Time", "Treatment")
+
+pheatmap(
+  global_heatmap_matrix,
+  cluster_rows = TRUE, # Groups genes with similar expression trends
+  cluster_cols = TRUE, # Groups samples with similar transcriptional profiles
+  show_colnames = FALSE, # Hide individual sample IDs for a cleaner look
+  annotation_col = metadata_annotation,
+  scale = "row", # Standardizes expression per gene (Z-score)
+  main = "Top 50 DE Genes: Global Experiment View",
+  color = colorRampPalette(c("blue", "white", "red"))(100)
+)
--- code/model.R
+++ code/model.R
@@ -44,59 +44,81 @@ colData(rse_gene_SRP127181)[, grepl(
 #SRR6392037                     24 hours           10nM estrogen
 #SRR6392038                     24 hours           10nM estrogen
 
-# Due we need this data in a format that can be used in the statistical model,
-#  we will convert the data to factors or numeric characters given the type of data
-#  we have in each column.
+# Since we need this data in a format that can be used in the statistical model,
+# we will convert the data to factors or numeric characters given the type of data
+# we have in each column.
 
-# sra_attribute.mutation_status and sra_attribute.source_name aren´t numerical characters , so it will convert to factors.
-rse_gene_SRP127181$sra_attribute.mutation_status <- factor(rse_gene_SRP127181$sra_attribute.mutation_status)
-rse_gene_SRP127181$sra_attribute.source_name <- factor(rse_gene_SRP127181$sra_attribute.source_name)
+# sra_attribute.mutation_status and sra_attribute.source_name are not numerical characters, so we will convert them to factors.
+rse_gene_SRP127181$sra_attribute.mutation_status <- factor(
+  rse_gene_SRP127181$sra_attribute.mutation_status
+)
+rse_gene_SRP127181$sra_attribute.source_name <- factor(
+  rse_gene_SRP127181$sra_attribute.source_name
+)
 
-# The variable sra_attribute.treatment_time has numeric characters, but have the word "hours" in it. Por razones practicas y para poder 
-# realizar fitExtractVarPartModel más adelante lo dejaremos como un factor. igual que las anteriores variables
-rse_gene_SRP127181$sra_attribute.treatment_time <- factor(rse_gene_SRP127181$sra_attribute.treatment_time)
+# The variable sra_attribute.treatment_time has numeric characters, but contains the word "hours" in it. For practical reasons and to be able to
+# perform fitExtractVarPartModel later, we will keep it as a factor, just like the previous variables.
+rse_gene_SRP127181$sra_attribute.treatment_time <- factor(
+  rse_gene_SRP127181$sra_attribute.treatment_time
+)
 
-# Checking the data of sra_attribute.treatment, it was notice that there was a typo in the word "ethanol", instead of "ethanol" it is written "ethonol".
-# The typo was known because searching in internet for "ethonol" does not give any quemical compound. 
+# Checking the data of sra_attribute.treatment, we noticed that there was a typo in the word "ethanol", instead of "ethanol" it was written "ethonol".
+# The typo was identified because searching on the internet for "ethonol" does not return any chemical compound.
 # Fix the typo and convert the data to factors.
-rse_gene_SRP127181$sra_attribute.treatment <- gsub("ethonol", "ethanol", rse_gene_SRP127181$sra_attribute.treatment)
-rse_gene_SRP127181$sra_attribute.treatment <- factor(rse_gene_SRP127181$sra_attribute.treatment)
- 
+rse_gene_SRP127181$sra_attribute.treatment <- gsub(
+  "ethonol",
+  "ethanol",
+  rse_gene_SRP127181$sra_attribute.treatment
+)
+rse_gene_SRP127181$sra_attribute.treatment <- factor(
+  rse_gene_SRP127181$sra_attribute.treatment
+)
+
 # Quality analysis of the data, using the variable assigned_gene_prop
-rse_gene_SRP127181$assigned_gene_prop <- rse_gene_SRP127181$recount_qc.gene_fc_count_all.assigned / rse_gene_SRP127181$recount_qc.gene_fc_count_all.total
+rse_gene_SRP127181$assigned_gene_prop <- rse_gene_SRP127181$recount_qc.gene_fc_count_all.assigned /
+  rse_gene_SRP127181$recount_qc.gene_fc_count_all.total
 summary(rse_gene_SRP127181$assigned_gene_prop)
 
-# Outut of the code above:
-#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# Output of the code above:
+#  Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
 # 0.7725  0.7917  0.7984  0.7981  0.8073  0.8140
-# Due the quality of the data is good, is not necessary to remove any sample from the analysis. 
-# Note: It is been decided that the quality of the data is goood because the minimum value of the variable assigned_gene_prop is 0.7725, 
+# Since the quality of the data is good, it is not necessary to remove any sample from the analysis.
+# Note: It has been decided that the quality of the data is good because the minimum value of the variable assigned_gene_prop is 0.7725,
 # which means that at least 77.25% of the reads were assigned to a CDS.
 
-# Calculate the median levels of gene expression in our samples 
+# Calculate the median levels of gene expression in our samples
 gene_means <- rowMeans(assay(rse_gene_SRP127181, "counts"))
 summary(gene_means)
 # Output of the code above:
-# Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
-#0.000e+00 0.000e+00 1.850e-01 5.463e+02 4.520e+01 2.655e+05 
-# It is notice that the mean of gene expression is higher than the median, which means that there are some genes with very high levels of expression that are affecting the mean.
-# Due to this, it is necessary to filter the data to remove the genes with very low levels of expression, because they are not informative for the analysis and can affect the results.
+# Min.   1st Qu.    Median      Mean   3rd Qu.      Max.
+#0.000e+00 0.000e+00 1.850e-01 5.463e+02 4.520e+01 2.655e+05
+# It is noticed that the mean of gene expression is higher than the median, which means that there are some genes with very high levels of expression that are affecting the mean.
+# Due to this, it is necessary to filter the data to remove genes with very low levels of expression, because they are not informative for the analysis and can affect the results.
 
 # Filter the data
-# Note:We will use the function filterByExpr from the edgeR package 
-# to filter the data, which uses a method based on the counts per million (CPM) to filter the data.
+# Note: We will use the function filterByExpr from the edgeR package
+# to filter the data, which uses a method based on counts per million (CPM) to filter the data.
 
 # Create a DGEList object with the data
 dge <- DGEList(counts = assay(rse_gene_SRP127181, "counts"))
 
 # Create a vector with the group information of the samples
 # Note: The group information is created by concatenating the variables sra_attribute.mutation_status, sra_attribute.treatment and sra_attribute.treatment_time, because these variables are the ones that we will use in the statistical model.
-group <- with(colData(rse_gene_SRP127181), paste0(sra_attribute.mutation_status, "_", sra_attribute.treatment, "_", sra_attribute.treatment_time))
+group <- with(
+  colData(rse_gene_SRP127181),
+  paste0(
+    sra_attribute.mutation_status,
+    "_",
+    sra_attribute.treatment,
+    "_",
+    sra_attribute.treatment_time
+  )
+)
 
 # Filter the data using the function filterByExpr
 keep <- filterByExpr(dge, group = group)
 
-# Filter the original RangedSummarizedExperiment 
+# Filter the original RangedSummarizedExperiment
 rse_gene_SRP127181_filtered <- rse_gene_SRP127181[keep, ]
 
 # Check the dimensions of the original and filtered data
@@ -111,33 +133,43 @@ dim(rse_gene_SRP127181_filtered)
 round(nrow(rse_gene_SRP127181_filtered) / nrow(rse_gene_SRP127181) * 100, 2)
 # Output of the code above:
 # [1] 33.31
-# This means that we have removed 66.69% of the genes from the original data, which is a good percentage of filtering, because we have removed the genes with very low levels of expression that are not informative for the analysis and can affect the results.
+# This means that we have removed 66.69% of the genes from the original data, which is a good filtering percentage, because we have removed the genes with very low levels of expression that are not informative for the analysis and can affect the results.
 
 # Normalizing the data
-# Note: Normalization is necessary to remove the technical variation in the data and to make the data comparable between samples. 
+# Note: Normalization is necessary to remove technical variation in the data and to make the data comparable between samples.
 
 # Note: The ColData is being used to map samples to metadata to link gene expression with experimental conditions for accurate statistical analysis.
-dge <- DGEList(counts = assay(rse_gene_SRP127181_filtered, "counts"), genes = rowData(rse_gene_SRP127181_filtered), samples = colData(rse_gene_SRP127181_filtered))
+dge <- DGEList(
+  counts = assay(rse_gene_SRP127181_filtered, "counts"),
+  genes = rowData(rse_gene_SRP127181_filtered),
+  samples = colData(rse_gene_SRP127181_filtered)
+)
 dge <- calcNormFactors(dge)
 
 # Exploring the data with the variancePartition package to see how the different variables are affecting the variance of the data.
-# Transform the data to log2 counts per million (logCPM) to estabilish variance 
+# Transform the data to log2 counts per million (logCPM) to establish variance
 # Note: VariancePartition requires the data to be in log2 counts per million (logCPM) format
 exp_log2 <- voom(dge)
 
-# Define the variables that we want to evaluate 
+# Define the variables that we want to evaluate
 # Note: Using the variable (1/variable) to evaluate the variance of each variable independently
-form_var <- ~ (1|sra_attribute.mutation_status) + (1|sra_attribute.treatment) + (1|(sra_attribute.treatment_time))
+form_var <- ~ (1 | sra_attribute.mutation_status) +
+  (1 | sra_attribute.treatment) +
+  (1 | (sra_attribute.treatment_time))
 
-# Calculate the partition of the variance 
+# Calculate the partition of the variance
 # vGene$E give the logCPM values of the data, form give the variables that we want to evaluate
-var_Part <- fitExtractVarPartModel(exp_log2$E, form_var, colData(rse_gene_SRP127181_filtered))
+var_Part <- fitExtractVarPartModel(
+  exp_log2$E,
+  form_var,
+  colData(rse_gene_SRP127181_filtered)
+)
 
 # Plot the partition of the variance to see how the different variables are affecting the variance of the data.
 plotVarPart(sortCols(var_Part))
 # As we can see in the plot, the variable sra_attribute.mutation_status is the one that is affecting the variance of the data the most,
-# afecting around 50% of the variance, the other variables do not affect the variance of the data as much. 
-# To know more about the variance between the different variables, a PCA is been used 
+# affecting around 50% of the variance, the other variables do not affect the variance of the data as much.
+# To know more about the variance between the different variables, a PCA is being used
 
 # Extract normalized counts using edgeR's cpm function
 counts_norm <- edgeR::cpm(dge, log = TRUE)
@@ -146,46 +178,66 @@ counts_norm <- edgeR::cpm(dge, log = TRUE)
 pca <- prcomp(t(counts_norm), scale. = TRUE)
 
 # Create data frame for plotting
-pca_df <- data.frame(  PC1 = pca$x[, 1], PC2 = pca$x[, 2],Mutation = rse_gene_SRP127181_filtered$sra_attribute.mutation_status)
+pca_df <- data.frame(
+  PC1 = pca$x[, 1],
+  PC2 = pca$x[, 2],
+  Mutation = rse_gene_SRP127181_filtered$sra_attribute.mutation_status
+)
 
 # Plot with color by mutation only
 ggplot(pca_df, aes(x = PC1, y = PC2, color = Mutation)) +
-geom_point(size = 4) +
-theme_bw() +
-labs(title = "PCA by Mutation Status", x = paste0("PC1 (", round(summary(pca)$importance[2, 1] * 100, 2), "%)"),y = paste0("PC2 (", round(summary(pca)$importance[2, 2] * 100, 2), "%)"))
+  geom_point(size = 4) +
+  theme_bw() +
+  labs(
+    title = "PCA by Mutation Status",
+    x = paste0("PC1 (", round(summary(pca)$importance[2, 1] * 100, 2), "%)"),
+    y = paste0("PC2 (", round(summary(pca)$importance[2, 2] * 100, 2), "%)")
+  )
 
-# It is noticed that the different mutation status are separated in the PCA plot, only the Wild Type is dividen in two groups, 
-# this could be due to another variable is affecting the variance of the data or to the batch effect.
-# To design our model matrix, it is being considered not only the variable sra_attribute.mutation_status, but also the variable sra_attribute.treatment_time because it is the second variable that is affecting the variance
-# This with the purpuse to have a goood model 
+# It is noticed that the different mutation statuses are separated in the PCA plot, only the Wild Type is divided into two groups,
+# this could be due to another variable affecting the variance of the data or to batch effect.
+# To design our model matrix, we are considering not only the variable sra_attribute.mutation_status, but also the variable sra_attribute.treatment_time because it is the second variable that is affecting the variance.
+# This is with the purpose of having a good model.
 
+# Before creating the model we need to reorder the levels
 
-# Antes de crear el modelo necesitamos reordenar los levels 
-
-# Note: Si observamos los levels de nuestra variable sra_attribute.mutation_status se observa lo sguiente:
+# Note: If we observe the levels of our variable sra_attribute.mutation_status, we see the following:
 # levels(rse_gene_SRP127181$sra_attribute.mutation_status)
 #[1] "ESR1 D538G" "ESR1 WT"    "ESR1 Y537S"
-# Si se crea el modelo con ese orden en los levels, entonces tendríamos a nuestra mutación "ESR1 D538G" como base, que 
-# sería lo que se compararía con la mutación "ESR1 Y537S" y el WT. En este caso, sería más interesante observar
-# la diferencia entre el WT y las dos mutaciones, por lo que necesitariamos que "ESR1 WT" fuera nuestra base. 
+# If we create the model with that order in the levels, then we would have our mutation "ESR1 D538G" as the base, which
+# would be what we compare with the mutation "ESR1 Y537S" and the WT. In this case, it would be more interesting to observe
+# the difference between the WT and the two mutations, so we would need "ESR1 WT" to be our base.
 
 levels(rse_gene_SRP127181_filtered$sra_attribute.mutation_status)
-rse_gene_SRP127181_filtered$sra_attribute.mutation_status <- relevel(rse_gene_SRP127181_filtered$sra_attribute.mutation_status,"ESR1 WT","ESR1 D538G","ESR1 Y537S")
-# El orden de nuestros levels queda así: 
+rse_gene_SRP127181_filtered$sra_attribute.mutation_status <- relevel(
+  rse_gene_SRP127181_filtered$sra_attribute.mutation_status,
+  "ESR1 WT",
+  "ESR1 D538G",
+  "ESR1 Y537S"
+)
+# The order of our levels is now:
 # [1] "ESR1 WT"    "ESR1 D538G" "ESR1 Y537S"
 
-# Create model 
-mod <- model.matrix(~ sra_attribute.mutation_status + sra_attribute.treatment_time + assigned_gene_prop, data = colData(rse_gene_SRP127181_filtered))
+# Create model
+mod <- model.matrix(
+  ~ sra_attribute.mutation_status +
+    sra_attribute.treatment_time +
+    assigned_gene_prop,
+  data = colData(rse_gene_SRP127181_filtered)
+)
 colnames(mod)
-# Output of the code above: 
+# Output of the code above:
 #[1] "(Intercept)"                             "sra_attribute.mutation_statusESR1 D538G" "sra_attribute.mutation_statusESR1 Y537S"
-#[4] "sra_attribute.treatment_time4"           "assigned_gene_prop"    
+#[4] "sra_attribute.treatment_time4"           "assigned_gene_prop"
 
-# Note: Gracias a "sra_attribute.treatment_time4" nos podemos dar cuenta que el tiempo que usaron como referencia es de las 24 hrs, en este caso 
-# no lo reordenaremos 
+# Note: Thanks to "sra_attribute.treatment_time4" we can realize that the time used as reference is 24 hours, in this case
+# we will not reorder it.
 
-# Guardando variables en carpeta de processed-data para posterior uso 
+# Save variables in processed-data folder for later use
 
-saveRDS(rse_gene_SRP127181_filtered, file = "processed-data/rse_gene_SRP127181_filtered")
+saveRDS(
+  rse_gene_SRP127181_filtered,
+  file = "processed-data/rse_gene_SRP127181_filtered"
+)
 saveRDS(dge, file = "processed-data/dge")
 saveRDS(mod, file = "processed-data/model_matrix")
